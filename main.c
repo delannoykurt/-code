95f0c36573e7a436b78cde296174bf1a05c412f4
delannoykurt <delannoykurt666@gmail.com>
2022-11-06 14:23:17 +0100

    add exercice random values
    random values, 0 at 10
    
    added a new programming exercise

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(void)
 {
-	puts("hello test");
-	puts("world ...");
+	int test;
+	
+	srand(time(NULL));
+	test = rand() % 10;
+
+	printf("valeure aleatoire de 0 a 10: %d\n", test);
+
+
 	return 0;
 }
